1cea11d2a4989e98df0b1a09debb6c5b12cfb9ae
Karlen <svaryan01@inbox.ru>
2022-05-24 14:52:02 +0300

    del redux hooks

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -1,9 +1,3 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from '../store/store';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
 export const getLocalStorage = () => {
   if (typeof window !== 'undefined') {
     return localStorage;
